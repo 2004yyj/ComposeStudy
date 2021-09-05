b1a0f4233012c38c558a79f00c289a3a7a713ecb
양윤재 <diddbswo04@dgsw.hs.kr>
2021-09-05 10:18:08 +0900

    [ADD] TextField

---
+++ textfield/src/main/java/kr/hs/dgsw/textfield/MainActivity.kt
@@ -0,0 +1,55 @@
+package kr.hs.dgsw.textfield
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.TextField
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import kr.hs.dgsw.textfield.ui.theme.ComposeStudyTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeStudyTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    EditText {
+                        Toast.makeText(this, it, Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun EditText(onValueChange: (String) -> Unit) {
+    var text by remember { mutableStateOf("") }
+    TextField(
+        value = text,
+        onValueChange = {
+            text = it
+            onValueChange(it)
+        },
+        modifier = Modifier.fillMaxWidth()
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposeStudyTheme {
+        EditText {
+            Log.d("MainActivity", "DefaultPreview: $it")
+        }
+    }
+}
